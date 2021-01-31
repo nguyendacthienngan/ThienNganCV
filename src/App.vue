be9fc910e2c9288ab7534ca75c8857589249a00a
Nguyen Dac Thien Ngan <54399397+nguyendacthienngan@users.noreply.github.com>
2021-02-01 00:24:00 +0700

    Deploy Firebase

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,13 @@ export default {
 }
 </script>
 
-<style src="./assets/styles/index.css" />
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
